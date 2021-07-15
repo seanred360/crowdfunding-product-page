e7c09626f59a40021953c33e157286319ecd63c4
Sean Redmon <seen360@yahoo.com>
2021-07-15 17:51:21 -0400

    Styling complete, before form validation

--- app/index.js
+++ app/index.js
@@ -1,15 +1,21 @@
-const btnHamburger = document.querySelector('.btnHamburger') // for mobile nav menu
-const hasFade = document.querySelectorAll('.has-fade') // .container__menu (mobile nav menu) and .gradient-overlay
-const hasFade2 = document.querySelectorAll('.has-fade-2') // the popup__window and the .dark__overlay
-const buttons = document.querySelectorAll('.button') // buttons that open the .popup__window to make a pledge
+const containerMenu = document.querySelector('.container__menu') // the mobile drop down menu
+const btnHamburger = document.querySelector('.btnHamburger') // opens the mobile headerLinks menu .container_menu
+const headerLinks = document.querySelector('.header__links') // desktop navigation links at the top
+const gradiantOverlay = document.querySelector('.gradiant-overlay') // a gradiant background behind the mobile menu
 const selectionLabels = document.querySelectorAll('.selection__label') // the label in .selection for .radio-input. Click to select a pledge reward
-const pledges = document.querySelectorAll('.pledge') // in .popup__window. Contains a text input and a button for making a pledge
-const popupWindow = document.querySelector('.popup__window')
-const darkOverlay = document.querySelector('.dark__overlay')
+const pledges = document.querySelectorAll('.pledge') // in .modal__window. Contains a text input and a button for making a pledge
+const modalWindow = document.querySelector('.modal__window') // a popup window with pledge options
+const buttons = document.querySelectorAll('.button') // buttons that open the .modal__window to make a pledge
+const darkOverlay = document.querySelector('.dark__overlay') // a transparent gray background
+const closeModal = document.querySelector('.close-modal') // X button to close .modal__window
 
 
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// ANIMATION TRIGGERS ////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// triggers a slide in or out animation on .pledge in _animations.scss
 const slidePledges = () => {
-    /// animate .pledge sliding in and out
     pledges.forEach(pledge => {
         pledge.classList.remove('slide-out')
         pledge.classList.add('slide-in')
@@ -21,20 +27,33 @@ const slidePledges = () => {
     }
 }
 
-const fadeInOut = target => {
-    target.forEach(e => {
-        if(!e.classList.contains('fade-in') && !e.classList.contains('fade-out')) {
-                e.classList.add('fade-in')
-            } else if(e.classList.contains('fade-in')) {
-                e.classList.remove('fade-in')
-                e.classList.add('fade-out')
-            } else if(e.classList.contains('fade-out')) {
-                e.classList.remove('fade-out')
-                e.classList.add('fade-in')
-            }
-    })
+// triggers a fade animation on the target element by adding a CSS class in _animations.scss
+function fadeInOut(target) {
+    if(!target.classList.contains('fade-in') && !target.classList.contains('fade-out')) {
+        target.classList.add('fade-in')
+    } else if(target.classList.contains('fade-in')) {
+        target.classList.remove('fade-in')
+        target.classList.add('fade-out')
+    } else if(target.classList.contains('fade-out')) {
+        target.classList.remove('fade-out')
+        target.classList.add('fade-in')
+    }
 }
 
+const toggleModalWindow = () => {
+    document.body.classList.toggle('no-scroll')
+    modalWindow.classList.toggle("active");
+    darkOverlay.classList.toggle("active");
+    fadeInOut(darkOverlay)
+    fadeInOut(modalWindow)
+    fadeInOut(headerLinks)
+};
+
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// RADIO INPUT ///////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// clear the .radio-input
 const clearSelect = () => {
     // clear the .radio-input that is currently selected
     const currentSelection = document.querySelector(".selection.active");
@@ -43,6 +62,7 @@ const clearSelect = () => {
         const pledge = document.querySelector(".selection.active .pledge");
         const currentInput = document.querySelector(".selection.active .pledge-input");
         currentSelection.classList.remove("active");
+        slidePledges()
         radio.checked = false;
         setTimeout(() => {
             currentInput.parentElement.classList.remove("error");
@@ -51,19 +71,18 @@ const clearSelect = () => {
     };
 };
 
+// select a new .radio-input
 const selectNew = select => {
-    const parentSelection = select.parentElement;
-    parentSelection.classList.add("active");
-    select.checked = true;
-    slidePledges();
-    setTimeout(() => select.scrollIntoView({ behavior: "smooth", block: "start" }), 500);
+    const parentSelection = select.parentElement; // .modal__window
+    parentSelection.classList.add("active"); // change the font color and border colors
+    select.checked = true; // check the .radio-input
+    slidePledges(); // show the .pledge
+    setTimeout(() => parentSelection.scrollIntoView({ behavior: "smooth"}), 550); // scroll .modal__window
 };
 
-const togglePopupWindow = () => {
-    popupWindow.classList.toggle("active");
-    darkOverlay.classList.toggle("active");
-    fadeInOut(hasFade2)
-};
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// CLICKABLE ELEMENTS ////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 
 selectionLabels.forEach(label => {
     label.addEventListener("click", () => {
@@ -74,58 +93,31 @@ selectionLabels.forEach(label => {
 
 buttons.forEach(b => {
     b.addEventListener("click", () => {
-        togglePopupWindow();
-        if (b.classList.contains("about__button")) {
-            //const inputID = b.getAttribute("href");
+        toggleModalWindow();
+        clearSelect()
+        if (b.classList.contains("about__button")) { // if it's a 'select reward' button
             const inputID = b.id;
             const checkedOption = document.querySelector(inputID);
             checkedOption.checked = true;
             selectNew(checkedOption);
-        };
+        } else {
+            modalWindow.scrollTop = 0; //it is the 'back this project' button, default to the top
+        }
     });
 });
 
 btnHamburger.addEventListener('click', () => {
     btnHamburger.classList.toggle('open')
-    fadeInOut(hasFade)
+    fadeInOut(gradiantOverlay)
+    fadeInOut(containerMenu)
 })
 
-// buttons open the popup window
-// buttons.forEach(button => {
-//     button.addEventListener('click', () => {
+closeModal.addEventListener('click', () => {
+    toggleModalWindow()
+})
 
-//         hasFade2.forEach(e => {
-//             if(!e.classList.contains('fade-in') && !e.classList.contains('fade-out')) {
-//                     e.classList.add('fade-in')
-//                 } else if(e.classList.contains('fade-in')) {
-//                     e.classList.remove('fade-in')
-//                     e.classList.add('fade-out')
-//                 } else if(e.classList.contains('fade-out')) {
-//                     e.classList.remove('fade-out')
-//                     e.classList.add('fade-in')
-//                 }
-//         })
-//     })
-// })
-
-// selectionLabels.forEach(label => {
-//     label.addEventListener('click', () =>{
-//         //remove active from all
-//         selectionLabels.forEach(item => {
-//             item.parentNode.classList.remove('active')
-//         })
-//         // select the label and put 'active' on the parent that controls the highlight styles for everything in the boxes
-//         label.parentNode.classList.add('active')
-
-//         /// animate .pledge sliding in and out
-//         pledges.forEach(pledge => {
-//             pledge.classList.remove('slide-out')
-//             pledge.classList.add('slide-in')
-//         })
-//         const selectionParent = label.parentNode
-//         const pledge = document.querySelector(".selection.active .pledge");
-//         pledge.classList.remove('slide-in')
-//         pledge.classList.add('slide-out')
-//         setTimeout(() => selectionParent.scrollIntoView({ behavior: "smooth", block: "start" }), 500);
-//     })
-// })
+// if the user clicks outside .modal__window while it is open, close .modal__window
+darkOverlay.addEventListener('click', () => {
+    clearSelect()
+    toggleModalWindow()
+})
